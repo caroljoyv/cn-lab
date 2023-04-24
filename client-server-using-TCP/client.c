90e41e3f566670f29319a84b1eb72c423d74f39e
caroljoyv <caroljoyv27@gmail.com>
2023-04-24 07:52:04 +0530

    updated server.c and client.c in TCP

--- client-server-using-TCP/client.c
+++ client-server-using-TCP/client.c
@@ -3,45 +3,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include<sys/socket.h>
- 
 int main(){
- exit
   char *ip = "127.0.0.1";
   int port = 5566;
- 
   int sock;
   struct sockaddr_in addr;
   socklen_t addr_size;
   char buffer[1024];
   int n;
- 
+
+  //socket creation
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0){
-    perror("[-]Socket error");
-    exit(1);
+  perror("[-]Socket error");
+  exit(1);
   }
   printf("[+]TCP server socket created.\n");
- 
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_port = port;
   addr.sin_addr.s_addr = inet_addr(ip);
- 
+
+  //connect
   connect(sock, (struct sockaddr*)&addr, sizeof(addr));
   printf("Connected to the server.\n");
- 
+
+  //sending
   bzero(buffer, 1024);
   strcpy(buffer, "HELLO, THIS IS CLIENT.");
   printf("Client: %s\n", buffer);
   send(sock, buffer, strlen(buffer), 0);
- 
+
+  //receiving
   bzero(buffer, 1024);
   recv(sock, buffer, sizeof(buffer), 0);
   printf("Server: %s\n", buffer);
- 
+
+  //close connection
   close(sock);
   printf("Disconnected from the server.\n");
- 
+  
   return 0;
 }
